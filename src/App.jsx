d3e6d9583b8a2a74281254ef157407c61158c592
Premkumar981 <premkumarsuru@gmail.com>
2025-07-15 21:37:02 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,46 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-// import Home from './components/Home.jsx';
-import Register from './components/Register.jsx';
-import Product from './components/Product.jsx';
-import Admin from './components/Admin.jsx';
-import Cart from './components/Cart.jsx';
-import Order from './components/Order.jsx';
-import Users from './components/Users.jsx';
-import Orders from './components/Orders.jsx';
-import Products from './components/Products.jsx';
-import Login from './components/Login.jsx';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-
+import { useState, createContext } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Register from "./components/Register";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import Order from "./components/Order";
+import Admin from "./components/Admin";
+import Users from "./components/Users";
+import Orders from "./components/Orders";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Products from "./components/Products";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+export const AppContext = createContext();
 function App() {
+  const [cart, setCart] = useState([]);
+  const [user, setUser] = useState({});
   return (
-    <div className='App-Container'>
-      <BrowserRouter>
-
-        {/* <h1>MERN Frontend</h1>
-        <Link to="/">Home</Link>-
-        <Link to="/cart">MyCart</Link>-
-        <Link to="/order">MyOrder</Link>-
-        <Link to="/login">Login</Link>-
-        <Link to="/admin">Admin</Link> */}
-
-<h1 className="main-title">MERN Frontend</h1>
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/cart">MyCart</Link>
-          <Link to="/order">MyOrder</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/admin">Admin</Link>
-        </div>
-
-        <Routes>
-          <Route index element={<Product/>}/>
-          <Route path='login' element={<Login/>}/>
-          <Route path='register' element={<Register/>}/>
-          <Route path='cart' element={<Cart/>}/>
-          <Route path='order' element={<Order/>}/>
-          <Route path='admin' element={<Admin/>}>
-            <Route index element={<Users/>}/>
-            <Route path='products' element={<Products/>}/>
-            <Route path='orders' element={<Orders/>}/>
-          </Route>
-        </Routes>
-        <h3>This is footer</h3>
-      </BrowserRouter>
+    <div className="App-Container">
+      <AppContext.Provider value={{ cart, setCart, user, setUser }}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route index element={<Product />} />
+            <Route path="login" element={<Login />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="register" element={<Register />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="order" element={<Order />} />
+            <Route path="admin" element={<Admin />}>
+              <Route index element={<Users />} />
+              <Route path="products" element={<Products />} />
+              <Route path="orders" element={<Orders />} />
+            </Route>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </AppContext.Provider>
     </div>
   );
 }
-
-export default App
+export default App;
