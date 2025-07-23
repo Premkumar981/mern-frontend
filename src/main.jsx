30df021e3459b8ca134278d72546a19d923b6b3f
Premkumar981 <premkumarsuru@gmail.com>
2025-07-23 09:26:37 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// pwa - mobile app
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+        updateSW(true);
+    }
+},
+onOfflineReady() {
+    console.log("App is ready to work offline.");
+}
+});
+
 createRoot(document.getElementById('root')).render(<App />);
